240d7762c9b79c3d456c89f9d1eb85b2be43502c
Steve Smith <steve@kentsmiths.com>
2019-08-11 11:47:01 -0400

    Adding method level samples

---
+++ RefactoringSamples/MethodLevel/IntentionObscuring/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RefactoringSamples.MethodLevel.IntentionObscuring
+{
+    public class Utilities
+    {
+        private readonly int iThsRte;
+        private readonly int iThsWkd;
+
+        public Utilities(int thsWkd, int thsRte)
+        {
+            iThsRte = thsRte;
+            iThsWkd = thsWkd;
+        }
+
+        public int m_otCalc()
+        {
+            return iThsWkd * iThsRte +
+                (int)Math.Round(0.5 * iThsRte * Math.Max(0, iThsWkd - 400));
+        }
+
+    }
+}
---
+++ RefactoringSamples/MethodLevel/IntentionObscuring/UtilitiesCharacterizationTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace RefactoringSamples.MethodLevel.IntentionObscuring
+{
+    public class UtilitiesCharacterizationTests
+    {
+        [Fact]
+        public void m_otCalcReturnsXGiven40and20()
+        {
+            var utilities = new Utilities(thsWkd: 40, thsRte: 20);
+
+            var result = utilities.m_otCalc();
+
+            Assert.Equal(0, result);
+        }
+    }
+}
