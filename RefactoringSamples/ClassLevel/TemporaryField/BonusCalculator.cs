2b1a0468467350ac862eee87d4f42c88b1accedd
Steve Smith <steve@kentsmiths.com>
2019-08-14 23:13:42 -0400

    Adding temp field

---
+++ RefactoringSamples/ClassLevel/TemporaryField/BonusCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace RefactoringSamples.ClassLevel.TemporaryField
+{
+    public class BonusCalculator
+    {
+        private Employee _employee;
+
+        public BonusCalculator(Employee employee)
+        {
+            _employee = employee;
+        }
+        public decimal CalculateBonus()
+        {
+            return CalculateEarningsForBonus() * _employee.BonusPercentage();
+        }
+        private decimal CalculateEarningsForBonus()
+        {
+            return _employee.YearToDateEarnings() + _employee.OvertimeEarnings() * 2;
+        }
+    }
+}
---
+++ RefactoringSamples/ClassLevel/TemporaryField/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefactoringSamples.ClassLevel.TemporaryField
+{
+    public class Employee
+    {
+        private decimal _earningsForBonus;
+
+        // other fields and methods  
+        private decimal CalculateBonus()
+        {
+            return _earningsForBonus * BonusPercentage();
+        }
+
+        public decimal BonusPercentage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateEarningsForBonus()
+        {
+            _earningsForBonus = YearToDateEarnings() + OvertimeEarnings() * 2;
+        }
+
+        public int YearToDateEarnings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int OvertimeEarnings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
